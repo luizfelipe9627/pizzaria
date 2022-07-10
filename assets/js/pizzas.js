4c7986707197a7fc1fe272861a0167a6371e1626
Luiz Felipe <felipesilva9627@hotmail.com>
2022-07-10 00:51:42 -0300

    Código JS comprimido para melhor desempenho.

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,58 +1,64 @@
 let pizzaJson = [
     {
-        id:1, 
-        name:'Mussarela', 
+        id: 1, 
+        name: 'Mussarela', 
         img: ('https://luizfelipe9627.github.io/pizzaria/assets/image/pizza1.webp'), 
-        price:20.19, 
-        sizes:['100g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        price: 20.19, 
+        sizes: ['100g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:2, 
-        name:'Calabresa',
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza2.webp', 
-        price:18.00, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 2, 
+        name: 'Calabresa',
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza2.webp', 
+        price: 18.00, 
+        sizes: ['320g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:3, 
-        name:'Quatro Queijos', 
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza3.webp', 
-        price:17.45, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 3, 
+        name: 'Quatro Queijos', 
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza3.webp', 
+        price: 17.45, 
+        sizes: ['320g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:4, 
-        name:'Americana', 
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza4.webp', 
-        price:19.77, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 4, 
+        name: 'Americana', 
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza4.webp', 
+        price: 19.77, 
+        sizes: ['320g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:5, 
-        name:'Sorvete', 
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza5.webp', 
-        price:21.43, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 5, 
+        name: 'Sorvete', 
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza5.webp', 
+        price: 21.43, 
+        sizes: ['320g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:6, 
-        name:'Moda da Casa', 
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza6.webp', 
-        price:18.55, sizes:['320g', '530g', '860g'], 
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 6, 
+        name: 'Moda da Casa', 
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza6.webp', 
+        price: 18.55, 
+        sizes: ['320g', '530g', '860g'], 
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     },
 
     {
-        id:7, 
-        name:'Chocolate', 
-        img:'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza7.webp', 
-        price:22.36, sizes:['320g', '530g', '860g'],
-        description:'Descrição da pizza em mais de uma linha muito legal bem interessante'
+        id: 7, 
+        name: 'Chocolate', 
+        img: 'https://luizfelipe9627.github.io/pizzaria/assets/image/pizza7.webp', 
+        price: 22.36, 
+        sizes: ['320g', '530g', '860g'],
+        description: 'Descrição da pizza em mais de uma linha muito legal bem interessante'
     }
 ];
